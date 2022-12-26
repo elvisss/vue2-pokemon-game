51ba03eaca2a9c07a6e88f50237ec37c5175cc8b
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2022-12-26 01:56:39 -0500

    fixing unit tests

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -25,9 +25,9 @@
 </template>
 
 <script>
-import PokemonPicture from '../components/PokemonPicture.vue'
-import PokemonOptions from '../components/PokemonOptions.vue'
-import getPokemonOptions from '../helpers/getPokemonOptions'
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+import getPokemonOptions from '@/helpers/getPokemonOptions'
 
 export default {
   components: {
